7c6157fee01d0f359a9d74dd6dc96e3805e84f2b
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2023-11-11 02:26:51 +0700

    Modified tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/screens/shoplist_form.dart';
 // import drawer widget
 import 'package:shopping_list/widgets/left_drawer.dart';
 import 'package:shopping_list/widgets/shop_card.dart';
 
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
   final List<ShopItem> items = [
-    ShopItem("View Products", Icons.checklist),
-    ShopItem("Add Product", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+    ShopItem("View Items", Icons.checklist, Colors.indigo.shade400),
+    ShopItem("Add Item", Icons.add_shopping_cart, Colors.blue.shade400),
+    ShopItem("Logout", Icons.logout, Colors.red.shade400),
   ];
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -74,60 +67,6 @@ class MyHomePage extends StatelessWidget {
                 }).toList(),
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {Key? key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.indigo,
-      child: InkWell(
-        // Area responsive to touch
-        onTap: () {
-          // Show SnackBar when clicked
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("You pressed the ${item.name} button!")));
-
-          // Navigate to the appropriate route (depending on the button type)
-          if (item.name == "Add Product") {
-            Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ShopFormPage(),
-                  ));
-          }
-        },
-        child: Container(
-          // Container to hold Icon and Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
           ),
         ),
       ),
